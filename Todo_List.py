b91c19e66572d7746a4fb6b0092949361087d6b5
Roy-Mutwiri <mutwiriroy63@gmail.com>
2023-05-16 20:36:47 +0300

    ""

--- Todo_List.py
+++ Todo_List.py
@@ -25,7 +25,6 @@ Label(root, image=noteImage, bg="#32405b").place(x=30, y=25)
 heading = Label(root, text="All Task", font="arial 20 bold", fg="white", bg="#32405b")
 heading.place(x=130, y=20)
 
-
 # Main
 
 frame = Frame(root, width=400, height=50, bg="white")
@@ -43,7 +42,8 @@ button.place(x=300, y=0)
 frame1 = Frame(root, bd=3, width=700, height=280, bg="#32405b")
 frame1.pack(pady=(160, 0))
 
-listbox = Listbox(frame1, font=("arial", 12), width=40, height=16, bg="#32405b", fg="white", cursor="hand2", selectbackground="#5a95ff")
+listbox = Listbox(frame1, font=("arial", 12), width=40, height=16, bg="#32405b", fg="white", cursor="hand2",
+                  selectbackground="#5a95ff")
 listbox.pack(side=LEFT, fill=BOTH, padx=2)
 scrollbar = Scrollbar(frame1)
 scrollbar.pack(side=RIGHT, fill=BOTH)
@@ -53,18 +53,8 @@ scrollbar.config(command=listbox.yview)
 
 # Delete
 
-Delete_icon = PhotoImage(file="/home/anon/Documents/Python Code/Todo_List/drive-download-20230516T165408Z-001/delete.png")
+Delete_icon = PhotoImage(
+    file="/home/anon/Documents/Python Code/Todo_List/drive-download-20230516T165408Z-001/delete.png")
 Button(root, image=Delete_icon, bd=0).pack(side=BOTTOM, pady=13)
-
-
-
-
-
-
-
-
-
-
-
 
 root.mainloop()
