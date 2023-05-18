9275dc38f5d6f4d9362aaa07947e4f9535d587a4
Roy-Mutwiri <mutwiriroy63@gmail.com>
2023-05-18 22:11:18 +0300

    ""

--- Todo_List.py
+++ Todo_List.py
@@ -107,6 +107,6 @@ Button(root, image=Delete_icon, bd=0, command=deleteTask).pack(side=BOTTOM, pady
 
 # Add Exit Button
 
-Ex = Button(root, text="Exit").pack(side=BOTTOM, pady=15)
+Ex = Button(root, text="Exit").pack(side=BOTTOM, pady=13)
 
 root.mainloop()
