aa6b4bd946098d049300dbc73e55f33c249a3fde
Roy-Mutwiri <mutwiriroy63@gmail.com>
2023-05-19 23:49:53 +0300

    ""

--- Todo_List.py
+++ Todo_List.py
@@ -61,13 +61,6 @@ def openTaskFile():
 with open("/home/anon/Documents/Python Code/Todo_List/tasklist.txt", "r") as file:
     todo_list = file.readlines()
 
-
-
-
-
-
-
-
 # Icon
 Image_Icon = PhotoImage(file="/home/anon/Documents/Python Code/Todo_List/drive-download-20230516T165408Z-001/task.png")
 root.iconphoto(False, Image_Icon)
